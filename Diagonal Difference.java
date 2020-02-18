6c0d3ff18cc496bb23c59edc740c1cf86dc97533
Ravi Gupta <ravigupta3282@gmail.com>
2020-02-18 17:24:46 +0530

    Solved

--- Diagonal Difference.java
+++ Diagonal Difference.java
@@ -5,7 +5,7 @@
     import java.util.*;
     import java.util.concurrent.*;
     import java.util.regex.*;
-
+  
     class Result {
         /*
          * Complete the 'diagonalDifference' function below.
